13c08ccc6ff6d9e4683fb1718d1b9eec258e9910
Emmanuel Quattropani <equattropani@gmail.com>
2020-06-07 11:15:03 -0300

    verssion 1.0.1

--- app.js
+++ app.js
@@ -1,77 +1,52 @@
-const {
-  app,
-  BrowserWindow, ipcMain
-} = require('electron')
-const url = require("url");
-const path = require("path");
-
+const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
 
-let appWindow
+let mainWindow;
 
-function initWindow() {
-  appWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  // Electron Build Path
-  appWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, `/dist/index.html`),
-      protocol: "file:",
-      slashes: true
-    })
-  );
-
-  // Initialize the DevTools.
-  //appWindow.webContents.openDevTools()
-
-  appWindow.on('closed', function () {
-    appWindow = null
-  })
-
-  appWindow.once('ready-to-show', () => {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadFile('dist/index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+  mainWindow.once('ready-to-show', () => {
     autoUpdater.checkForUpdatesAndNotify();
   });
 }
 
+app.on('ready', () => {
+  createWindow();
+});
 
-
-app.on('ready', initWindow)
-
-// Close when all windows are closed.
 app.on('window-all-closed', function () {
-
-  // On macOS specific close process
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', function () {
-  if (win === null) {
-    initWindow()
+  if (mainWindow === null) {
+    createWindow();
   }
-})
-
-
+});
 
 ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
 
-
 autoUpdater.on('update-available', () => {
-  appWindow.webContents.send('update_available');
-});
-autoUpdater.on('update-downloaded', () => {
-  appWindow.webContents.send('update_downloaded');
+  mainWindow.webContents.send('update_available');
 });
 
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
 
 ipcMain.on('restart_app', () => {
   autoUpdater.quitAndInstall();
